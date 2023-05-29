54d5a47d654b40213fae78db9f82c91eee12ac73
Shiv <shivang.shukla@vernost.in>
2023-05-29 14:35:21 +0530

    ver

--- config/sequelize.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize('freindster', 'root', '9026724930M@n', {
- host: 'localhost',
- dialect: 'mysql',
- port: "3306"
-});
-
-module.exports = sequelize;
--- index.js
+++ index.js
@@ -1,22 +1,18 @@
 const express = require('express')
-const routes = require('./routes/routes');
-const sequelize = require("./config/sequelize")
 
 const app = express()
-app.use(express.json());
 const PORT = 4000
-app.use('/api', routes);
-
-
-sequelize.sync().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
-})
-
 
 app.get('/', (req, res) => {
-  res.send('Hey this is my API')
+ res.send('Hey this is my API running 🥳')
+})
+
+app.get('/about', (req, res) => {
+ res.send('This is my about route..... ')
+})
+
+app.listen(PORT, () => {
+ console.log(`API listening on PORT ${PORT} `)
 })
 
 
